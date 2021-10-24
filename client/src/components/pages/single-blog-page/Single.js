74d2c53f57577d4091ec1ad3512d3e468a3ec75e
sia162 <nowitissiya@gmail.com>
2021-10-24 18:51:08 +0530

    single-blog-page responsiveness done

--- client/src/components/pages/single-blog-page/Single.js
+++ client/src/components/pages/single-blog-page/Single.js
@@ -1,6 +1,7 @@
 import "./single.css";
 import Sidebar from "../../sidebar/Sidebar";
 import SingleBlog from "../../single-blog/SingleBlog";
+import "../../../responsive/single-blog-page-responsive.css";
 
 const Single = () => {
   return (
